a35a8c2da8295f59dea4ce14fed334cf898e1161
Pantelis Sopasakis <p.sopasakis@gmail.com>
2020-05-13 19:03:41 +0100

    refactored code added setup.py and manifest file

--- crust_casadi/parser.py
+++ crust_casadi/parser.py
@@ -1,10 +1,13 @@
 from pycparser import c_ast, c_generator, parse_file
+import re
+__all__ = ['Crust']
 
 
 class Parser:
 
-    def __init__(self, fname):
-        ast = parse_file(fname, use_cpp=True, cpp_path='clang',
+    def __init__(self, filename):
+        # TODO: Change -Iutils/fake - use pkg_resources
+        ast = parse_file(filename, use_cpp=True,
                          cpp_args=['-E', r'-Iutils/fake'])
         self.ast = ast
 
@@ -19,5 +22,98 @@ class Parser:
 
 class Crust:
 
-    def __init__(self):
-        pass
+    __CRUST_TYPES = {'double': 'f64',
+                     'int': 'i32',
+                     'long': 'i64'}
+    __CRUST_DEFAULT_INIT = {'double': 0.0, 'int': 0}
+    __CRUST_FUNCTION_MAP = {'cos': 'f64::cos',
+                            'sin': 'f64::sin',
+                            'tan': 'f64::tan',
+                            'sqrt': 'f64::sqrt'}
+
+    def __init__(self, file_name, function_name):
+        parser = Parser(file_name)
+        self.f = parser.get_function_by_name(function_name)
+        self.code = None
+
+    @staticmethod
+    def __crust_function_mapping(fname):
+        p = re.compile('[a-z_]+_[a-z]+_fmax', re.IGNORECASE)
+        if p.match(fname) is not None:
+            return 'f64::max'
+        return Crust.__CRUST_FUNCTION_MAP[fname]
+
+    @staticmethod
+    def __declaration_to_rust(dec):
+        var_name = dec.name
+        var_type = dec.type.type.names[0]
+        var_init = Crust.__CRUST_DEFAULT_INIT[var_type] if dec.init is None else dec.init.value
+        rust_type = Crust.__CRUST_TYPES[var_type]
+        return 'let mut %s: %s = %s;' % (var_name, rust_type, var_init)
+
+    @staticmethod
+    def __assignment_to_rust(dec):
+        var_name = dec.lvalue.name
+        rhs = dec.rvalue
+        stmt = var_name + ' = '
+        generator = c_generator.CGenerator()
+        if isinstance(rhs, c_ast.BinaryOp):
+            operator = rhs.op
+            op_left = rhs.left.name
+            op_right = rhs.right.name
+            if operator in ['<=', '>=', '==', '<', '>']:
+                stmt += 'if %s %s %s {1.0} else {0.0}' \
+                        % (op_left, operator, op_right)
+            else:
+                stmt += op_left + operator + op_right
+        elif isinstance(rhs, c_ast.FuncCall):
+            fname = rhs.name.name
+            p = re.compile('[a-z_]+_[a-z]+_sq', re.IGNORECASE)
+            if p.match(fname) is not None:
+                stmt += 'f64::powi(' + rhs.args.exprs[0].name + ', 2)'
+            else:
+                fname = Crust.__crust_function_mapping(fname)
+                args = ",".join([x.name for x in rhs.args.exprs])
+                stmt += '%s(%s)' % (fname, args)
+        elif isinstance(rhs, c_ast.Constant):
+            stmt += generator.visit(rhs)
+        elif isinstance(rhs, c_ast.TernaryOp):
+            stmt += generator.visit(rhs.iftrue)
+        elif isinstance(rhs, c_ast.UnaryOp):
+            if rhs.op == '!':
+                rhs_var = rhs.expr.name
+                stmt += " if f64::abs(%s) < f64::EPSILON { 1.0 } else { 0.0 }" \
+                        % rhs_var
+        return stmt + ';'
+
+    @staticmethod
+    def __ifblock_to_rust(dec):
+        generator = c_generator.CGenerator()
+        return generator.visit(dec.iftrue) + ';'
+
+    @staticmethod
+    def __crust(blocks):
+        code = []
+        for node in blocks:
+            if isinstance(node, c_ast.Decl):
+                stmt = Crust.__declaration_to_rust(node)
+            elif isinstance(node, c_ast.Assignment):
+                stmt = Crust.__assignment_to_rust(node)
+            elif isinstance(node, c_ast.If):
+                stmt = Crust.__ifblock_to_rust(node)
+            elif isinstance(node, c_ast.Return):
+                stmt = "return 0;"
+            code += [stmt]
+        return code
+
+    def parse(self):
+        function = self.f
+        node_body = function.body
+        self.code = Crust.__crust(blocks=node_body.block_items)
+
+    def to_rust_file(self, file_name):
+        with open(file_name, "w") as fh:
+            fh.write("#[allow(unused_assignments)]\n"
+                     "fn casadi(arg: &[&[f64]], res: &mut [&mut [f64]]) -> u16 {\n\t")
+            fh.write("\n\t".join(self.code))
+            fh.write("\n}\n")
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import io
+import os
+
+NAME = 'crust_casadi'
+VERSION = '0.0.0'
+DESCRIPTION = 'C-to-Rust Transpiler for CasADi C files'
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+setup(name=NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author=['Pantelis Sopasakis'],
+      author_email='p.sopasakis@gmail.com',
+      license='MIT License',
+      packages=find_packages(
+            exclude=["tests", "c", "rust"]),
+      include_package_data=True,
+      install_requires=[
+          'pycparser'
+      ],
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'License :: OSI Approved :: MIT License'
+            'Programming Language :: Python',
+            'Programming Language :: Rust',
+            'Intended Audience :: Science/Research',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: Mathematics',
+            'Topic :: Software Development :: Code Generators',
+            'Topic :: Software Development :: Embedded Systems'
+      ],
+      keywords=['transpiler', 'crust', 'casadi'],
+      url=(
+            'https://github.com/alphaville/optimization-engine'
+      ))
