38391b3b95e1d609685bc881f01792272a3cdbce
Pantelis Sopasakis <p.sopasakis@gmail.com>
2020-05-14 22:02:49 +0100

    travis ci: install rust

--- ci/install.sh
+++ ci/install.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 
 main() {
     # Install necessary stuff
+    rustup toolchain remove stable && rustup toolchain install stable
     sudo pip install --upgrade pip
     sudo pip install --upgrade virtualenv
     rustc --version
